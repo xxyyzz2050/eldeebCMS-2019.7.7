1f29b27653f83b357130a14b34ea9a14405fee5b
Xx159753@@ <xxyyzz2050@gmail.com>
2019-06-29 15:42:22 +0300

    generate: articles, error
    > ng g c error --module app
    
    > ng g m articles --routing
    > ng g c articles/article --module articles
    > ng g c articles/index --module articles
    > ng g c articles/manage --module articles

---
+++ src/app/articles/articles.module.tns.ts
@@ -0,0 +1,17 @@
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+
+import { ArticlesRoutingModule } from './articles-routing.module';
+import { NativeScriptCommonModule } from 'nativescript-angular/common';
+import { ArticleComponent } from './article/article.component';
+import { IndexComponent } from './index/index.component';
+import { ManageComponent } from './manage/manage.component';
+
+@NgModule({
+  declarations: [ArticleComponent, IndexComponent, ManageComponent],
+  imports: [
+    ArticlesRoutingModule,
+    NativeScriptCommonModule
+  ],
+  schemas: [NO_ERRORS_SCHEMA]
+})
+export class ArticlesModule { }

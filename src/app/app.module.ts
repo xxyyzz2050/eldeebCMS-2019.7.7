ff5829f9030e79cdbe71969bb0b0c587de7ee740
Xx159753@@ <xxyyzz2050@gmail.com>
2019-06-29 13:49:15 +0300

    firebase: add AngularFire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { firebase } from "config";
+import { AngularFireModule } from "@angular/fire"; //firebase core (required)
+import { AngularFireAuthModule } from "@angular/fire/auth"; //example of adding additional firebase products
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserModule.withServerTransition({ appId: "serverApp" }),
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(firebase, "my-app-name"),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]

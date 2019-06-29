1b109235b1e9377860ecb9ae2e50ad9f9831bce3
Xx159753@@ <xxyyzz2050@gmail.com>
2019-06-29 12:53:55 +0300

    format code (run prettier & tslint)
    > npm run code

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -5,23 +5,23 @@ or other file it requires
 from: https://github.com/angular/angular-cli/issues/7200
  */
 
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
 
 //this plugin makes webpack use paths that defined in tsconfig.ts
 //https://github.com/dividab/tsconfig-paths-webpack-plugin
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
-  mode: "none",
+  mode: 'none',
   entry: {
     // This is our Express server for Dynamic universal
-    server: "./server.ts"
+    server: './server.ts'
   },
-  target: "node",
+  target: 'node',
   resolve: {
-    extensions: [".ts", ".js"],
-    modules: [path.resolve("."), "node_modules"],
+    extensions: ['.ts', '.js'],
+    modules: [path.resolve('.'), 'node_modules'],
     plugins: [new TsconfigPathsPlugin()]
   },
   optimization: {
@@ -29,14 +29,14 @@ module.exports = {
   },
   output: {
     // Puts the output at the root of the dist folder
-    path: path.join(__dirname, "dist"),
-    filename: "[name].js",
-    library: "app",
-    libraryTarget: "umd"
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    library: 'app',
+    libraryTarget: 'umd'
   },
   module: {
     rules: [
-      { test: /\.ts$/, loader: "ts-loader" },
+      { test: /\.ts$/, loader: 'ts-loader' },
       {
         // Mark files inside `@angular/core` as using SystemJS style dynamic imports.
         // Removing this will cause deprecation warnings to appear.
@@ -49,13 +49,13 @@ module.exports = {
     new webpack.ContextReplacementPlugin(
       // fixes WARNING Critical dependency: the request of a dependency is an expression
       /(.+)?angular(\\|\/)core(.+)?/,
-      path.join(__dirname, "src"), // location of your src
+      path.join(__dirname, 'src'), // location of your src
       {} // a map of your routes
     ),
     new webpack.ContextReplacementPlugin(
       // fixes WARNING Critical dependency: the request of a dependency is an expression
       /(.+)?express(\\|\/)(.+)?/,
-      path.join(__dirname, "src"),
+      path.join(__dirname, 'src'),
       {}
     ) /*
 //ReferenceError: window is not defined  https://github.com/ui-router/angular/issues/134#issuecomment-310699942

1f417e7af88202c71d7d64b297b69f217f657f98
Xx159753@@ <xxyyzz2050@gmail.com>
2019-06-29 16:20:12 +0300

    setup routes for articles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ErrorComponent } from "./error/error.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "articles",
+    loadChildren: () =>
+      import("./articles/articles.module").then(m => m.ArticlesModule)
+  },
+
+  { path: "", redirectTo: "", pathMatch: "full" },
+  { path: "**", component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
